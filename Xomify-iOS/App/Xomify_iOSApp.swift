afcb152fd61aa159cfe8639dc631e1740f99fe06
Dominick Giordano <dominickgiordano@Dominicks-MacBook-Pro.local>
2026-01-02 13:53:49 -0500

    First commit

---
+++ Xomify-iOS/App/Xomify_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xomify_iOSApp.swift
+//  Xomify-iOS
+//
+//  Created by Dominick Giordano on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Xomify_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xomify-iOS/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xomify-iOS
+//
+//  Created by Dominick Giordano on 1/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
